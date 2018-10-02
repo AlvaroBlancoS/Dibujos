9966d0f8a29925a84bb65d8bb7bd0f2803eba3e8
ablanco <alvaroblanconsangines.to@gmail.com>
2018-10-02 20:45:57 +0200

    Como Hacer dibujitos

---
+++ src/Sonrie/Gatito.java
@@ -0,0 +1,26 @@
+package Sonrie;
+
+public class Gatito {
+	
+	
+	String number; 
+	static String n = "Issa";
+public static void main(String[] args) {
+	// Gatito
+	int number = 7;
+	 
+	
+	System.out.println("   /\\       /\\");		
+	System.out.println("  /  \\_____/  \\");
+	System.out.println(" /             \\");
+	System.out.println("(   /\\     /\\   )");
+	System.out.println("====    v    ====");
+	System.out.println("=====(__|__)=====");
+	System.out.println(" (             )");
+	System.out.println("  (___________)");
+	
+	System.out.println("Un gatito tiene"+" "+ number +" "+"vidas" );
+
+}
+
+}
---
+++ src/Sonrie/PorFavor.java
@@ -0,0 +1,17 @@
+package Sonrie;
+
+public class PorFavor {
+	
+	public static void main(String[] args) {
+	
+		System.out.println("  ####  ");
+		System.out.println(" #    # ");
+		System.out.println("# #  # #");
+		System.out.println("#      #");
+		System.out.println("# #  # #");
+		System.out.println("#  ##  #");
+		System.out.println(" #    # ");
+		System.out.println("  ####  ");
+		System.out.println("Sonríe por favor!!!!!!");
+	}
+}
